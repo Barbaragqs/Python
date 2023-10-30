64e5d4290cff75a4288c4a33ec85e73cf7ea38f1
Barbaragqs <133073020+Barbaragqs@users.noreply.github.com>
2023-10-30 16:07:12 -0300

    first commit

---
+++ funcao_aula4.py
@@ -0,0 +1,18 @@
+'''
+#paradigma = forma de se resolver problemas em linguagem de programação
+#função é pegar algo abstrato e materializar em forma de programação
+
+modelo:
+
+def nome_funcao(parametros):
+    comandos
+
+'''
+
+def saudacao(nome):
+    return(f"boa tarde {nome.title()}!")
+
+nome = input("digite seu nome: ")
+resultado = saudacao(nome)
+print(resultado)
+
