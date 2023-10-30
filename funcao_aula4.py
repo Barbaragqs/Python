d557a5c71e718a4e8e417d219d54aa6b1e908f07
Barbaragqs <133073020+Barbaragqs@users.noreply.github.com>
2023-10-30 16:39:09 -0300

    exemplo 2

--- funcao_aula4.py
+++ funcao_aula4.py
@@ -6,8 +6,7 @@ modelo:
 
 def nome_funcao(parametros):
     comandos
-
-'''
+ex 1:
 
 def saudacao(nome):
     return(f"boa tarde {nome.title()}!")
@@ -15,4 +14,18 @@ def saudacao(nome):
 nome = input("digite seu nome: ")
 resultado = saudacao(nome)
 print(resultado)
+'''
+
+#criar função que recebe nome e idade e retorna uma mensagem de cadastro com o nome e idade informado
+
+def cadastro(n,i):
+    return f"O usuário {n} com idade {i} foi cadastrado com sucesso"
+nome = input ("digite seu nome: ")
+idade = int(input("Digite sua idade:"))
+if(idade>45):
+    resultado = cadastro (nome, idade)
+    print (resultado)
+else: print("idade não aceita")
+
+
 
